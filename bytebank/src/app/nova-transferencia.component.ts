72a6ada1cd75995f8466374491e828c615bdce71
Guilherme Lima <glima@noovi.com.br>
2022-01-19 17:02:35 -0300

    Trabalhando no CSS e Manipulando a DOM

--- bytebank/src/app/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia.component.ts
@@ -1,11 +1,19 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'app-nova-transferencia',
-    templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia.component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss']
 })
 
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
 
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitada nova transferencia');
+    console.log('valor: ', this.valor);
+    console.log('destino: ', this.destino);
+  }
 }

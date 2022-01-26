93c328417f947cd4ffb58701b6341f1536a9a88b
Guilherme Lima <glima@noovi.com.br>
2022-01-26 18:36:00 -0300

    Trabalhando Com Rotas da Página

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NovaTransferenciaComponent } from './nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
@@ -21,6 +22,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    AppRoutingModule
   ],
   providers: [
     {provide: LOCALE_ID, useValue: 'pt' },
